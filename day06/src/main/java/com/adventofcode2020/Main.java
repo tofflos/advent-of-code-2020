f220764804ceb0161e85320a52b121944cc10fe5
Erik Östlund <erik.ostlund@gmail.com>
2020-12-06 09:08:42 +0100

    Minor polish

--- day06/src/main/java/com/adventofcode2020/Main.java
+++ day06/src/main/java/com/adventofcode2020/Main.java
@@ -11,27 +11,20 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        var input = Files.readString(Paths.get("6.in"));
-
-        var part1 = Arrays.stream(input.split("\n\n"))
-                .map(s -> s.replace("\n", ""))
+        var frequencies = Arrays.stream(Files.readString(Paths.get("6.in")).split("\n\n"))
                 .map(s -> s.split(""))
-                .mapToLong(arr -> Arrays.stream(arr).distinct().count())
-                .sum();
+                .map(arr -> Arrays.stream(arr).collect(Collectors.groupingBy(Function.identity(), Collectors.counting())))
+                .collect(Collectors.toList());
 
-        System.out.println("Part 1: " + part1);
+        System.out.println("Part 1: " + frequencies.stream()
+                        .mapToLong(answers -> answers.entrySet().stream().filter(e -> !"\n".equals(e.getKey())).count())
+                        .sum());
 
-        var part2 = Arrays.stream(input.split("\n\n"))
-                .map(s -> s.split(""))
-                .map(s -> Arrays.stream(s).collect(Collectors.groupingBy(Function.identity(), Collectors.counting())))
-                .mapToLong(answers -> {
-                    var groupSize = answers.getOrDefault("\n", 0L) + 1;
-                    return answers.entrySet().stream()
-                            .filter(e -> !"\n".equals(e.getKey()) && groupSize == e.getValue())
-                            .count();
-                })
-                .sum();
-
-        System.out.println("Part 2: " + part2);
+        System.out.println("Part 2: " + frequencies.stream()
+                        .mapToLong(answers -> {
+                            var groupSize = answers.getOrDefault("\n", 0L) + 1;
+                            return answers.entrySet().stream().filter(e -> !"\n".equals(e.getKey()) && groupSize == e.getValue()).count();
+                        })
+                        .sum());
     }
 }
