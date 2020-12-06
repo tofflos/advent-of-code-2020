6e24c734a359fa36334e69c12c6cf62caa87b3fb
Erik Östlund <erik.ostlund@gmail.com>
2020-12-06 11:29:43 +0100

    Minor polish

--- day06/src/main/java/com/adventofcode2020/Main.java
+++ day06/src/main/java/com/adventofcode2020/Main.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -11,20 +12,24 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        var frequencies = Arrays.stream(Files.readString(Paths.get("6.in")).split("\n\n"))
-                .map(s -> s.split(""))
-                .map(arr -> Arrays.stream(arr).collect(Collectors.groupingBy(Function.identity(), Collectors.counting())))
+        var groups = Arrays.stream(Files.readString(Paths.get("6.in")).split("\n\n"))
+                .map(Group::fromString)
                 .collect(Collectors.toList());
 
-        System.out.println("Part 1: " + frequencies.stream()
-                        .mapToLong(answers -> answers.entrySet().stream().filter(e -> !"\n".equals(e.getKey())).count())
-                        .sum());
+        System.out.println("Part 1: " + groups.stream()
+                .mapToInt(g -> g.answerFrequencies().size())
+                .sum());
 
-        System.out.println("Part 2: " + frequencies.stream()
-                        .mapToLong(answers -> {
-                            var groupSize = answers.getOrDefault("\n", 0L) + 1;
-                            return answers.entrySet().stream().filter(e -> !"\n".equals(e.getKey()) && groupSize == e.getValue()).count();
-                        })
-                        .sum());
+        System.out.println("Part 2: " + groups.stream()
+                .mapToLong(g -> g.answerFrequencies().entrySet().stream().filter(e -> e.getValue() == g.groupSize()).count())
+                .sum());
+    }
+}
+
+record Group(long groupSize, Map<String, Long> answerFrequencies) {
+
+    static Group fromString(String s) {
+        return new Group(s.lines().count(),
+                Arrays.stream(s.replace("\n", "").split("")).collect(Collectors.groupingBy(Function.identity(), Collectors.counting())));
     }
 }
