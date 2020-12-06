9535cad76a35a68b72aa0b60586cb1ee969a5b04
Erik Östlund <erik.ostlund@gmail.com>
2020-12-06 08:42:43 +0100

    Minor polish

--- day06/src/main/java/com/adventofcode2020/Main.java
+++ day06/src/main/java/com/adventofcode2020/Main.java
@@ -11,23 +11,22 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        var answers = Files.readString(Paths.get("6.in"));
+        var input = Files.readString(Paths.get("6.in"));
         
-        var part1 = Arrays.stream(answers.split("\n\n"))
+        var part1 = Arrays.stream(input.split("\n\n"))
                 .map(s -> s.replace("\n", ""))
                 .map(s -> s.split(""))
                 .mapToLong(s -> Arrays.stream(s).distinct().count()).sum();
 
         System.out.println("Part 1: " + part1);
 
-        var part2 = Arrays.stream(answers.split("\n\n"))
+        var part2 = Arrays.stream(input.split("\n\n"))
                 .map(s -> s.split(""))
                 .map(s -> Arrays.stream(s).collect(Collectors.groupingBy(Function.identity(), Collectors.counting())))
-                .mapToLong(group -> {
-                    long persons = group.getOrDefault("\n", 0L) + 1;
-                    return group.entrySet().stream()
-                            .filter(e -> !"\n".equals(e.getKey()) && persons == e.getValue())
-                            .mapToLong(e -> e.getValue())
+                .mapToLong(answers -> {
+                    var groupSize = answers.getOrDefault("\n", 0L) + 1;
+                    return answers.entrySet().stream()
+                            .filter(e -> !"\n".equals(e.getKey()) && groupSize == e.getValue())
                             .count();
                 }).sum();
 
