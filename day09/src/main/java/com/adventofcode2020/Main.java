c61ade25882d7f27d86daae570f25e5ef3d91d2c
Erik Östlund <erik.ostlund@gmail.com>
2020-12-09 22:20:30 +0100

    Used queue, window and streams for part 1

--- day09/src/main/java/com/adventofcode2020/Main.java
+++ day09/src/main/java/com/adventofcode2020/Main.java
@@ -4,9 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayDeque;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -17,56 +15,52 @@ public class Main {
                 .map(Long::parseLong)
                 .collect(Collectors.toList());
 
-        var preamble = 25;
+        var size = 25;
 
-        var weakness1 = part1(numbers, preamble);
-        var weakness2 = part2(numbers, preamble, weakness1);
+        var weakness1 = part1(numbers, size);
+        var weakness2 = part2(numbers, size, weakness1);
 
         System.out.println("Part 1: " + weakness1);
         System.out.println("Part 2: " + weakness2);
     }
 
-    static long part1(List<Long> numbers, int preamble) {
-        for (int i = 0; i < numbers.size(); i++) {
-            if (!sums(numbers.subList(i, i + preamble)).contains(numbers.get(i + preamble))) {
-                return numbers.get(i + preamble);
+    static long part1(List<Long> numbers, int size) {
+        var queue = new ArrayDeque<Long>(numbers.subList(size, numbers.size()));
+        var window = new ArrayDeque<Long>(numbers.subList(0, size));
+
+        while(!queue.isEmpty()) {
+            var q = queue.pop();
+            
+            if(window.stream()
+                    .flatMap(w1 -> window.stream()
+                            .map(w2 -> w1 + w2))
+                    .noneMatch(q::equals)) {
+                return q;
             }
+            
+            window.pop();
+            window.add(q);
         }
 
         throw new IllegalArgumentException();
-    }
-
-    static Set<Long> sums(List<Long> numbers) {
-        var sums = new HashSet<Long>();
-
-        for (int i = 0; i < numbers.size(); i++) {
-            for (int j = 0; j < numbers.size(); j++) {
-                if (i != j) {
-                    sums.add(numbers.get(i) + numbers.get(j));
-                }
-            }
-        }
-
-        return sums;
     }
 
     static long part2(List<Long> numbers, int premable, long weakness) {
         var queue = new ArrayDeque<Long>();
 
         for (var number : numbers) {
-
             queue.add(number);
 
             var sum = queue.stream().mapToLong(Long::valueOf).sum();
 
             while (sum > weakness) {
                 queue.poll();
-                sum = queue.stream().mapToLong(Long::valueOf).sum();
+                sum = queue.stream().mapToLong(Long::longValue).sum();
             }
 
             if (sum == weakness) {
-                var min = queue.stream().mapToLong(Long::valueOf).min().orElseThrow();
-                var max = queue.stream().mapToLong(Long::valueOf).max().orElseThrow();
+                var min = queue.stream().mapToLong(Long::longValue).min().orElseThrow();
+                var max = queue.stream().mapToLong(Long::longValue).max().orElseThrow();
                 return min + max;
             }
         }
