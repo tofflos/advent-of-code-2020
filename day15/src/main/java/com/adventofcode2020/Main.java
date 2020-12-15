67ce50c33486fd902be9025bb747a9836c9b2078
Erik Östlund <erik.ostlund@gmail.com>
2020-12-15 11:52:16 +0100

    Added solution for day 15 part 2

--- day15/src/main/java/com/adventofcode2020/Main.java
+++ day15/src/main/java/com/adventofcode2020/Main.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -12,18 +14,32 @@ public class Main {
 
         var numbers = Arrays.stream(Files.readString(Paths.get("15.in")).split(",")).map(Integer::parseInt).collect(Collectors.toList());
 
-        for (int i = numbers.size(); i < 2020; i++) {
-            var previous = numbers.get(i - 1);
+        System.out.println("Part 1: " + game(numbers, 2020));
+        System.out.println("Part 2: " + game(numbers, 30000000));
+    }
 
-            var t = numbers.subList(0, numbers.size() - 1).lastIndexOf(previous);
+    static int game(List<Integer> numbers, int turns) {
+        var seen1 = new HashMap<Integer, Integer>();
+        var seen2 = new HashMap<Integer, Integer>();
 
-            if (t == -1) {
-                numbers.add(0);
-            } else {
-                numbers.add(i - 1 - t);
-            }
+        for (int i = 0; i < numbers.size(); i++) {
+            seen1.put(numbers.get(i), i);
         }
 
-        System.out.println("Part 1: " + numbers.get(numbers.size() - 1));
+        for (int i = numbers.size(); i < turns; i++) {
+            var previous = numbers.get(i - 1);
+            var t1 = seen1.getOrDefault(previous, -1);
+            var t2 = seen2.getOrDefault(previous, -1);
+            var current = t1 == -1 || t2 == -1 ? 0 : t1 - t2;
+
+            if (seen1.containsKey(current)) {
+                seen2.put(current, seen1.get(current));
+            }
+
+            seen1.put(current, i);
+            numbers.add(current);
+        }
+        
+        return numbers.get(numbers.size() - 1);
     }
 }
