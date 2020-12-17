f47b49c4ef2e030d24395300422f130dbd90485e
Erik Östlund <erik.ostlund@gmail.com>
2020-12-17 20:06:27 +0100

    Added solution for day 17 part 2

--- day17/src/main/java/com/adventofcode2020/Main.java
+++ day17/src/main/java/com/adventofcode2020/Main.java
@@ -12,12 +12,12 @@ public class Main {
 
         var input = Files.readAllLines(Paths.get("17.in"));
 
-        var bounds = new Bounds(0, input.get(0).length(), 0, input.size(), 0, 1);
+        var bounds = new Bounds(0, input.get(0).length(), 0, input.size(), 0, 1, 0, 1);
         var current = new HashMap<Coordinate, Character>();
 
         for (int y = bounds.y1(); y < bounds.y2(); y++) {
             for (int x = bounds.x1(); x < bounds.x2(); x++) {
-                current.put(new Coordinate(x, y, 0), input.get(y).charAt(x));
+                current.put(new Coordinate(x, y, 0, 0), input.get(y).charAt(x));
             }
         }
 
@@ -26,21 +26,23 @@ public class Main {
         for (int cycle = 0; cycle < 6; cycle++) {
             var next = new HashMap<Coordinate, Character>();
 
-            for (int z = bounds.z1(); z < bounds.z2(); z++) {
-                for (int y = bounds.y1(); y < bounds.y2(); y++) {
-                    for (int x = bounds.x1(); x < bounds.x2(); x++) {
-                        var coordinate = new Coordinate(x, y, z);
-                        var state = current.getOrDefault(coordinate, '.');
-                        var neighbours = neighbours(current, coordinate);
-                        var n = neighbours.values().stream().filter(c -> c == '#').count();
+            for (int w = bounds.w1(); w < bounds.w2(); w++) {
+                for (int z = bounds.z1(); z < bounds.z2(); z++) {
+                    for (int y = bounds.y1(); y < bounds.y2(); y++) {
+                        for (int x = bounds.x1(); x < bounds.x2(); x++) {
+                            var coordinate = new Coordinate(x, y, z, w);
+                            var state = current.getOrDefault(coordinate, '.');
+                            var neighbours = neighbours(current, coordinate);
+                            var n = neighbours.values().stream().filter(c -> c == '#').count();
 
-                        if (state == '#') {
-                            next.put(coordinate, n == 2 || n == 3 ? '#' : '.');
-                        } else if (state == '.') {
-                            next.put(coordinate, n == 3 ? '#' : '.');
+                            if (state == '#') {
+                                next.put(coordinate, n == 2 || n == 3 ? '#' : '.');
+                            } else if (state == '.') {
+                                next.put(coordinate, n == 3 ? '#' : '.');
+                            }
                         }
-                    }
 
+                    }
                 }
             }
 
@@ -48,7 +50,7 @@ public class Main {
             bounds = bounds.expand();
         }
 
-        System.out.println("Part 1: " + current.values().stream().filter(c -> c == '#').count());
+        System.out.println("Part 2: " + current.values().stream().filter(c -> c == '#').count());
     }
 
     static Map<Coordinate, Character> neighbours(Map<Coordinate, Character> coordinates, Coordinate coordinate) {
@@ -57,10 +59,12 @@ public class Main {
         for (int y = coordinate.y() - 1; y <= coordinate.y() + 1; y++) {
             for (int x = coordinate.x() - 1; x <= coordinate.x() + 1; x++) {
                 for (int z = coordinate.z() - 1; z <= coordinate.z() + 1; z++) {
-                    var neighbour = new Coordinate(x, y, z);
+                    for (int w = coordinate.w() - 1; w <= coordinate.w() + 1; w++) {
+                        var neighbour = new Coordinate(x, y, z, w);
 
-                    if (!neighbour.equals(coordinate)) {
-                        neighbours.put(neighbour, coordinates.getOrDefault(neighbour, '.'));
+                        if (!neighbour.equals(coordinate)) {
+                            neighbours.put(neighbour, coordinates.getOrDefault(neighbour, '.'));
+                        }
                     }
                 }
             }
@@ -70,13 +74,13 @@ public class Main {
     }
 }
 
-record Bounds(int x1, int x2, int y1, int y2, int z1, int z2) {
+record Bounds(int x1, int x2, int y1, int y2, int z1, int z2, int w1, int w2) {
 
     Bounds expand() {
-        return new Bounds(x1 - 1, x2 + 1, y1 - 1, y2 + 1, z1 - 1, z2 + 1);
+        return new Bounds(x1 - 1, x2 + 1, y1 - 1, y2 + 1, z1 - 1, z2 + 1, w1 - 1, w2 + 1);
     }
 }
 
-record Coordinate(int x, int y, int z) {
+record Coordinate(int x, int y, int z, int w) {
 
 }
