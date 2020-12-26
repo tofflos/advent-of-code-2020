f64260bb24de58c156c1bf0cfa0c2c492157a239
Erik Östlund <erik.ostlund@gmail.com>
2020-12-26 11:15:30 +0100

    Added solution for day 19 part 2

--- day19/src/main/java/com/adventofcode2020/Main.java
+++ day19/src/main/java/com/adventofcode2020/Main.java
@@ -21,7 +21,41 @@ public class Main {
                 .map(arr -> Map.entry(Integer.parseInt(arr[0]), arr[1]))
                 .collect(Collectors.toMap(e -> e.getKey(), e -> e.getValue()));
         var messages = input[1].lines().collect(Collectors.toList());
+        var materializations = materialize(rules);
 
+        System.out.println("Part 1: " + messages.stream()
+                .filter(s -> materializations.get(0).contains(s))
+                .count());
+
+        System.out.println("Part 2: " + messages.stream()
+                .filter(s -> materializations.get(0).contains(s) || extendedValidation(s, materializations.get(42), materializations.get(31)))
+                .count());
+    }
+
+    static boolean extendedValidation(String message, Set<String> materializations42, Set<String> materializations31) {
+        // Materialization42 and materalization31 contain strings that are 8 characters long and have no elements in common.
+        // The sequence of binary numbers, when strings are interpreted as a=0 and b=1, does not match anything at https://oeis.org.
+        // Shape of message must match the pattern 42x + 42y + 31y
+
+        var length = materializations31.stream().findAny().orElseThrow().length();
+        var count31 = 0;
+        var count42 = 0;
+        var t = message;
+
+        while (t.length() >= length && materializations31.contains(t.substring(t.length() - length))) {
+            count31++;
+            t = t.substring(0, t.length() - length);
+        }
+
+        while (t.length() >= length && materializations42.contains(t.substring(0, length))) {
+            count42++;
+            t = t.substring(length);
+        }
+
+        return count31 > 0 && count42 > count31 && count42 * length + count31 * length == message.length();
+    }
+
+    static HashMap<Integer, Set<String>> materialize(Map<Integer, String> rules) {
         var materializations = new HashMap<Integer, Set<String>>();
 
         while (!rules.isEmpty()) {
@@ -36,7 +70,7 @@ public class Main {
                             .map(s -> Arrays.stream(s.split(" ")).map(Integer::parseInt).map(materializations::get).collect(Collectors.toList()))
                             .collect(Collectors.toList());
 
-                    var combinations = strings.stream().map(Main::combinations).collect(Collectors.toList());
+                    var combinations = strings.stream().map(Main::combine).collect(Collectors.toList());
 
                     materializations.put(entry.getKey(), combinations.stream().flatMap(t -> t.stream()).collect(Collectors.toSet()));
                 }
@@ -45,10 +79,10 @@ public class Main {
             materializations.keySet().forEach(rules::remove);
         }
 
-        System.out.println("Part 1: " + messages.stream().filter(s -> materializations.get(0).contains(s)).count());
+        return materializations;
     }
 
-    static Set<String> combinations(List<Set<String>> strings) {
+    static Set<String> combine(List<Set<String>> strings) {
         var current = new HashSet<String>(strings.get(0));
 
         for (int i = 1; i < strings.size(); i++) {
@@ -64,6 +98,5 @@ public class Main {
         }
 
         return current;
-
     }
 }
